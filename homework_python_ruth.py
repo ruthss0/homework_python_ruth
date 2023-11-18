90643704e0709266af48f2b5552cb712e9bc5730
Gabi Vawdrey <ruth.sousa15@gmail.com>
2023-11-17 20:26:18 -0700

    homework file

---
+++ homework_python_ruth.py
@@ -0,0 +1,94 @@
+homework_python_ruth
+
+
+#ruth homework
+
+# 1 - Which of the following is not a basic Python data type?
+# Text
+########################################################
+# 2 - What is the output for the following code?
+
+# def say_hello():
+#   print("hello")
+
+#C - Nothing– the function is not being called.
+
+########################################################
+
+# 3 - What is the output for the following code?
+
+# my_hobby = "coding"
+# your_hobby = "dancing"
+
+# if my_hobby == your_hobby:
+#     print("We can code together!")
+# else:
+#     print("We can try your hobby first!")
+
+# C - "We can try your hobby first!" is printed.
+
+########################################################
+
+# 4 - Is indentation meaningful in Python syntax?
+
+# C - Yes– if you don’t include proper indentation there will be an error.
+
+########################################################
+
+# 5 - What is the output for the following code?
+
+# name = "Brighticorn"
+# name2 = name.upper()
+# print(f"{name2} thinks you're cool.")
+
+# D - "BRIGHTICORN thinks you're cool." is printed.
+
+########################################################
+
+# Coding Practice
+
+# 1 Write a function that prints congratulations on one line, and then prints a smiley face on the second line. Don’t forget to call your function to test that it works.#
+
+# def find_reaction():
+#   print("congratulations")
+#   print(":)")
+
+# find_reaction()
+
+########################################################
+#2  Write a while loop that counts to 5.
+
+# count = 1
+# while count <=5:
+#   print(count)
+#   count+=1
+
+########################################################
+
+# 3 Write a function that asks the user for their name and then greets the user using their name. Don’t forget to call your function to test that it works.
+
+# def user_name():
+#   name = input("Whats your name?")
+#   print(f"Hey {name}!")
+
+# user_name()
+
+########################################################
+
+# 4 Write a while loop that continually asks a user for a number between 50 and 100.
+
+
+
+# while True:
+#   user_ask = int(input("write  a number between 50 and 100: "))
+
+#   if user_ask > 100:
+#     print("Too high!")
+#   elif user_ask < 50:
+#     print("Too low!")
+  
+#   else:
+#     print("Great")
+#     break
+    
+
